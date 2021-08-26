2e3e388a84e9362c4ae75a57ddb403168942f4e5
JuHoKim <gnb3792@gmail.com>
2021-08-26 18:10:20 +0900

    change1

--- run/run.sh
+++ run/run.sh
@@ -6,10 +6,10 @@ args=(
 python ../main.py "${args[@]}"
 
 
-# args=(
-#     -name ResNeXt -bs 4 -epoch 1 -augment t
-#     -use_clf_l T -clf_l aam -loss_scale 32 -loss_margin .25 -use_metric_l f 
-#     -module_name model_ResNeXt -model_name get_ResNeXt 
-#     -m_dsp F -m_up_path F -m_gate F
-#     )
-# python ../main.py "${args[@]}"
+args=(
+    -name ResNeXt -bs 4 -epoch 1 -augment t
+    -use_clf_l T -clf_l aam -loss_scale 32 -loss_margin .25 -use_metric_l f 
+    -module_name model_ResNeXt -model_name get_ResNeXt 
+    -m_dsp F -m_up_path F -m_gate F
+    )
+python ../main.py "${args[@]}"
